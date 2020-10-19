1140098d4a58194ab7a7672f17429f3cda588194
rodrigocamargo854@gmail.com <rodrigocamargo854@gmail.com>
2020-10-19 07:26:49 -0300

    Day 3 adicionados orphanage.js, add-file.svg,create-orphanage.html

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -36,3 +36,33 @@ const icon = L.icon({
 //create and add marker              
 L.marker([-26.905457, -49.036230], { icon })
     .addTo(map)
+
+
+// galeriia de imagem
+// image galery
+function selectImage(event) {
+
+    // Botão clicado
+    const button = event.currentTarget
+
+    // remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    buttons.forEach(removeActiveClass)
+
+    // selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+
+    // atualizar o container de imagem
+    imageContainer.src = image.src
+
+    // add a classe .active para o botão clicado
+    button.classList.add('active')
+
+}
